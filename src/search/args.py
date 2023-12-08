f019132ed1a2c52a5488d404d3693d266dc4cb06
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2023-12-08 13:23:56 +0330

    Update args.py

--- src/search/args.py
+++ src/search/args.py
@@ -13,12 +13,12 @@ from torch.utils.data import DataLoader, Dataset, random_split
 
 from copy import copy
 
-sys.path.insert(0, '/kaggle/working/RobustDARTS')
+sys.path.insert(0, '/kaggle/working/OurDARTS')
 from src import utils
 
 class Parser(object):
     def __init__(self):
-        parser = argparse.ArgumentParser("RobustDARTS")
+        parser = argparse.ArgumentParser("OurDARTS")
 
         # general options
         parser.add_argument('--data', type=str, default='/kaggle/input/novelmindscientistsdataset/MRI')
@@ -57,7 +57,7 @@ class Parser(object):
         parser.add_argument('--drop_path_prob',          type=float,          default=0.2,            help='drop path probability')
 
         # logging options
-        parser.add_argument('--save',                    type=str,            default='/kaggle/working/RobustDARTS/experiments/search_logs',  help='experiment name')
+        parser.add_argument('--save',                    type=str,            default='/kaggle/working/OurDARTS/experiments/search_logs',  help='experiment name')
         parser.add_argument('--results_file_arch',       type=str,            default='/kaggle/working/results_arch', help='filename where to write architectures')
         parser.add_argument('--results_file_perf',       type=str,            default='/kaggle/working/results_perf', help='filename where to write val errors')
         parser.add_argument('--report_freq',             type=float,          default=50,             help='report frequency')
