127edaa63ee9a4749827e68139c8b89e7a2d9c57
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-01-11 14:17:51 +0330

    Update DARTS_search.sh

--- scripts/DARTS_search.sh
+++ scripts/DARTS_search.sh
@@ -16,5 +16,5 @@
 #
 
 # source activate pytorch-0.3.1-cu8-py36
-python /kaggle/input/OurDarts/src/search/train_search.py --unrolled --job_id 1 --task_id 1 --seed 2 --cutout --report_freq_hessian 2 --space $1 --dataset $2 --drop_path_prob $3 --weight_decay $4
+python /kaggle/input/ourdarts/OurDarts-main/src/search/train_search.py --unrolled --job_id 1 --task_id 1 --seed 2 --cutout --report_freq_hessian 2 --space $1 --dataset $2 --drop_path_prob $3 --weight_decay $4
 
