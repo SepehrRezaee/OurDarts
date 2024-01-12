ffc609851185963b8127a6325f8f008a2543c969
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-01-13 00:00:38 +0330

    Update architect.py

--- src/search/architect.py
+++ src/search/architect.py
@@ -14,7 +14,7 @@ class Architect(object):
     self.network_momentum = args.momentum
     self.network_weight_decay = args.weight_decay
     self.model = model
-    self.optimizer = torch.optim.Adam(self.model.arch_parameters(),
+    self.optimizer = torch.optim.Adam(self.model.module.arch_parameters(),
         lr=args.arch_learning_rate, betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
 
   def _compute_unrolled_model(self, input, target, eta, network_optimizer):
